38eb4c1f5bac978cccd78593730bc1680ec3a982
Chiappi03 <99718163+Chiappi03@users.noreply.github.com>
2022-02-18 13:56:57 +0100

    Add files via upload

---
+++ Programmazione_Facile.js
@@ -0,0 +1,204 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Che significa l’acronimo HTML?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+  numb: 2,
+  question: "Che significa l’acronimo CSS?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  
+{
+  numb: 3,
+  question: "Che significa l’acronimo JS?",
+  answer: "Java Script",
+  options: [
+    "Joint System",
+    "Java System",
+    "Joint Security",
+    "Java Script"
+  ]
+},
+ 
+{
+  numb: 4,
+  question: "Che significa l’acronimo SQL?",
+  answer: "Structured Query Language",
+  options: [
+    "Structured Query Language",
+    "Structured Quantum Language",
+    "System Query Local",
+    "Structured Query Local"
+  ]
+},
+ 
+{
+  numb: 5,
+  question: "La differenza tra SQL e mySQL?",
+  answer: "SQL è un linguaggio con cui interrogare un database,mySQL è un sistema che aiuta a gestire i database relazionali",
+  options: [
+    "SQL è un liguaggio di programmazione mentre mySQL è il formato con cui vengono salvatii lavori  ",
+    "SQL è un linguaggio con cui interrogare un database,mySQL è un sistema che aiuta a gestire i database relazionali",
+    "sono uguali",
+    "mySQL è un linguaggio con cui interrogare un database, SQL è un sistema che aiuta a gestire i database relazionali"
+  ]
+},
+ 
+{
+  numb: 6,
+  question: "Che cos’è una Query string?",
+  answer: "è la parte di URL che contiene i dati da passare in input ad un programma",
+  options: [
+    "è la barra di ricerca",
+    "è una stringa volatile",
+    "è la parte di URL che contiene i dati da passare in input ad un programma",
+    "è una stringa senza nome"
+  ]
+},
+ 
+{
+  numb: 7,
+  question: "Cos’è un puntatore?",
+  answer: "è una variabile che contiene l'indirizzo di memoria di un'altra variabile",
+  options: [
+    "oggetto di una variabile",
+    "una variabile senza indirizzo ",
+    "un metodo usato per salvare i file",
+    "è una variabile che contiene l'indirizzo di memoria di un'altra variabile"
+  ]
+},
+ 
+{
+  numb: 8,
+  question: "Differenze tra GET e POST?",
+  answer: "Il metodo Get viene utilizzato principalmente allo scopo di visualizzare . Il metodo Post viene utilizzato soprattutto a scopo di aggiornamento",
+  options: [
+    "Il metodo Get viene utilizzato principalmente allo scopo di visualizzare . Il metodo Post viene utilizzato soprattutto a scopo di aggiornamento",
+    "il metodo Get è usato per la ricerca dei valori e il metodo Post per mostrare i risultati",
+    "non ci sono differenze ",
+    "il metodo Get si usa per aggiornare e il metodo Post viene usato per visualizzare"
+  ]
+},
+ {
+    numb: 9,
+    question: "In quali campi viene utilizzato Python?",
+    answer: "è un linguaggio adatto a tutti i campi",
+    options: [
+      "nel campo informatico",
+      "campi elettronici",
+      "è un linguaggio adatto a tutti i campi",
+      "nelle librerie"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Che cos’è il  Dev-C++?",
+    answer: "Dev-C++ è un IDE gratuito",
+    options: [
+      "Dev-C++ è un IDE gratuito",
+      "un linguaggio di programmazione",
+      "una variabile",
+      "è una casella di testo"
+    ]
+  },
+    {
+    numb: 11,
+    question: "Che cos’è un’ IDE?",
+    answer: "è un ambiente di sviluppo nel quale  viene eseguito il codice e visualizza gli errori ",
+    options: [
+      "un motore di ricerca",
+      "un sistema operativo",
+      "è un ambiente di sviluppo nel quale  viene eseguito il codice e visualizza gli errori ",
+      "un formato di file"
+    ]
+  },
+  
+  {
+    numb: 12,
+    question: "La differenza tra un IDE ed un editor di testo?",
+    answer: "Un editor di testo è utilizzato per modificare il testo normale mentre l’IDE è un editor di testo, un editor di codice, un debugger, un compilatore ",
+    options: [
+      "Un editor di testo è utilizzato per modificare il testo normale mentre l’IDE è un editor di testo, un editor di codice, un debugger, un compilatore ",
+      "nessuna",
+      "l'editor di testo non permettte la modifica del messaggio mentre l'IDE si ",
+      "L'IDE non permette l'aggiornamento dei dati mentre l'editor di testo si "
+    ]
+  },
+   
+  {
+    numb: 13,
+    question: "Cosa fa il comando “printf()” in C?",
+    answer: "stampa il contenuto tra parentesi",
+    options: [
+      "ti permette di visualizzare gli errori ",
+      "effettua un ciclo for",
+      "interroga il database",
+      "stampa il contenuto tra parentesi"
+    ]
+  },
+   
+  {
+    numb: 14,
+    question: "Come si conclude un comando in C?",
+    answer: "si conclude con il ;",
+    options: [
+      "non serve la punteggiatura",
+      "basta un .",
+      "basta andare a capo",
+      "si conclude con il ;"
+    ]
+  },
+   
+  {
+    numb: 15,
+    question: "Che cos’è una libreria?",
+    answer: "è un insieme di funzioni predefinite che devono essere importate nel programma tramite il comando #include",
+    options: [
+      "una libreia è una varibile contenente i valori prima inseriti",
+      "è un insieme di funzioni predefinite che devono essere importate nel programma tramite il comando #include",
+      "una libraria è un insieme di varibili ",
+      "è un altro nome del puntatore"
+    ]
+  },
+   
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 16,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4",
+  //     "option 5",
+  //     "option 6",
+  //     "option 7",
+  //     "option 8",
+  //     "option 9",
+  //     "option 10",
+  //     "option 11",
+  //     "option 12",
+  //     "option 13",
+  //     "option 14",
+  //     "option 15"
+  //   ]
+  // },
+];
